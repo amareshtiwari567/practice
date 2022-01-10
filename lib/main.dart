917f95636740d18c9e50966b5d62e9db16bca2d5
Your Name <amareshtiwri567@gmail.com>
2022-01-11 03:32:12 +0530

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,29 @@
 import 'package:flutter/material.dart';
 
-void main(List<String> args) {}
+void main() {
+  runApp(MaterialApp(
+    title: "Demo",
+    home: Homepage(),
+  ));
+}
+
+class Homepage extends StatelessWidget {
+  const Homepage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Demo Application"),
+        centerTitle: true,
+      ),
+      body: Container(
+        child: Center(
+            child: Text(
+          "Amaresh Tiwari",
+        )),
+        color: Colors.black26,
+      ),
+    );
+  }
+}
