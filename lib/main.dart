5bbaabf8535416a67c8ce039d1c89b04e107cbac
Your Name <amareshtiwri567@gmail.com>
2022-01-13 01:31:03 +0530

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
+import 'dart:js';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+// main function of the program
 void main() {
   runApp(MaterialApp(
     title: "Demo",
@@ -7,22 +11,36 @@ void main() {
   ));
 }
 
+// stateless widgets
 class Homepage extends StatelessWidget {
   const Homepage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      // scaffold used for predefined templets
+      // used for Appbar
       appBar: AppBar(
+        // This is the Title of the Application which will show the top of the AppBar
         title: const Text("Demo Application"),
+        // Title will be situated in the center
         centerTitle: true,
       ),
-      body: Container(
-        child: Center(
-            child: Text(
-          "Amaresh Tiwari",
-        )),
-        color: Colors.black26,
+      // This is the body of the Application
+      body: Center(
+        //  Body holds the Child in the application bar
+        child: Container(
+          // these are the attribute of the container like padding,allignment,width,height
+          padding: EdgeInsets.all(8),
+          alignment: Alignment.center,
+          width: 100,
+          height: 100,
+          // thi
+          decoration: BoxDecoration(
+              // color: Colors.blueAccent,
+              ),
+          child: const Text("Amaresh Tiwari"),
+        ),
       ),
     );
   }
