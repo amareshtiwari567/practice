3753c801e2b33d7656d6c7c18041609fbc569e80
Your Name <amareshtiwri567@gmail.com>
2022-01-13 02:06:08 +0530

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -40,7 +40,7 @@ class Homepage extends StatelessWidget {
             color: Colors.blueAccent,
           ),
           // this is used for  TextArea
-          child: const Text(" Amaresh Tiwari"),
+          child: Text("    Amaresh Tiwari   "),
         ),
       ),
     );
