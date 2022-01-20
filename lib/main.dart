85a3bc83ea098f94c157d17762691212d4942b1f
Your Name <amareshtiwri567@gmail.com>
2022-01-21 01:36:35 +0530

    image

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,3 @@
-import 'dart:html';
-import 'dart:js';
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -21,6 +18,7 @@ class Homepage extends StatelessWidget {
   Widget build(BuildContext context) {
     // ignore: dead_code
     return Scaffold(
+      backgroundColor: Colors.amber,
       // scaffold used for predefined templets
       // used for Appbar
       appBar: AppBar(
@@ -30,7 +28,31 @@ class Homepage extends StatelessWidget {
         centerTitle: true,
       ),
       // This is the body of the Application
-      body: Container(),
+      body: Center(
+        child: Card(
+          child: Column(
+            children: <Widget>[
+              Image.asset(
+                "assets/bg.jpeg",
+                fit: BoxFit.cover,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: SizedBox(
+                  height: 50,
+                  child: Text(
+                    "Change",
+                    style: TextStyle(
+                        fontSize: 35,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.red),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
       drawer: Padding(
         padding: const EdgeInsets.all(8.0),
         child: Drawer(
@@ -87,5 +109,16 @@ class Homepage extends StatelessWidget {
         child: Icon(Icons.edit),
       ),
     );
+  }
+}
+
+class TextField extends StatelessWidget {
+  const TextField({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField();
   }
 }
